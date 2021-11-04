2233d06b7f95ed0fe0ef0a5fd479b4c22e6f9365
Adr205 <A01242469@itesm.mx>
2021-11-03 23:26:11 -0600

    movieTest

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { MovieTagComponent } from './movie-tag/movie-tag.component';
+
 const routes: Routes = [
   {path: '', component: HeaderComponent},
+  {path: 'movie', component:MovieTagComponent }
 ];
 
 @NgModule({
---
+++ src/app/movie-tag/movie-tag.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-tag',
+  templateUrl: './movie-tag.component.html',
+  styleUrls: ['./movie-tag.component.scss']
+})
+export class MovieTagComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
